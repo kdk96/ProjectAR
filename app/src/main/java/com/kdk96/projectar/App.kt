8b5bdaf9e80381ddba7db79388b361e77b0e6d02
Dias Kinayatov <kdk96s@gmail.com>
2018-08-19 03:09:20 +0700

    app module created

---
+++ app/src/main/java/com/kdk96/projectar/App.kt
@@ -0,0 +1,24 @@
+package com.kdk96.projectar
+
+import android.app.Application
+import com.kdk96.common.di.ChildComponents
+import com.kdk96.common.di.ComponentDependenciesProvider
+import com.kdk96.common.di.ComponentManager
+import com.kdk96.common.di.HasComponentDependencies
+import com.kdk96.projectar.di.AppComponent
+import com.kdk96.projectar.di.DaggerAppComponent
+import javax.inject.Inject
+
+class App : Application(), ComponentManager, HasComponentDependencies {
+    private lateinit var appComponent: AppComponent
+    @Inject
+    override lateinit var components: ChildComponents
+    @Inject
+    override lateinit var dependencies: ComponentDependenciesProvider
+
+    override fun onCreate() {
+        super.onCreate()
+        appComponent = DaggerAppComponent.builder().context(this).build()
+        appComponent.inject(this)
+    }
+}
