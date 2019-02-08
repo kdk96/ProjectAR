d4b9c4c71ba078d9766823393e8fe9c2cd7bedd9
Dias Kinayatov <kdk96s@gmail.com>
2019-02-08 17:01:05 +0600

    database vacuum on init

--- database/src/main/java/com/kdk96/database/Database.kt
+++ database/src/main/java/com/kdk96/database/Database.kt
@@ -1,13 +1,16 @@
 package com.kdk96.database
 
 import android.arch.persistence.room.Database
+import android.arch.persistence.room.DatabaseConfiguration
 import android.arch.persistence.room.RoomDatabase
+import android.database.sqlite.SQLiteDatabase
 import com.kdk96.database.dao.CompanyDao
 import com.kdk96.database.dao.PrizeDao
 import com.kdk96.database.dao.QuestDao
 import com.kdk96.database.entity.Company
 import com.kdk96.database.entity.Prize
 import com.kdk96.database.entity.Quest
+import java.io.File
 
 @Database(
         entities = [
@@ -22,4 +25,16 @@ abstract class Database : RoomDatabase() {
     abstract fun companyDao(): CompanyDao
     abstract fun questDao(): QuestDao
     abstract fun prizeDao(): PrizeDao
+
+    override fun init(configuration: DatabaseConfiguration) {
+        val dbPath = configuration.context.getDatabasePath(configuration.name).path
+        if (dbExists(dbPath)) {
+            val db = SQLiteDatabase.openDatabase(dbPath, null, 0)
+            db.execSQL("VACUUM")
+            db.close()
+        }
+        super.init(configuration)
+    }
+
+    private fun dbExists(dbPath: String) = File(dbPath).exists()
 }
